784b40f5b1526bc3f46ced1652cfe8ccbb2775dd
Masum-Arifin <masumarifinbd@gmail.com>
2023-02-27 16:01:36 +0600

    setup react-redux

---
+++ src/redux/todos/actionTypes.js
@@ -0,0 +1,6 @@
+export const ADDED = 'todos/ADDED';
+export const TOGGLED = 'todos/TOGGLED';
+export const COLORSELECTED = 'todos/colorSelected';
+export const DELETED = 'todos/DELETED';
+export const ALLCOMPLETED = 'todos/allCompleted';
+export const CLEARCOMPLETED = 'todos/clearCompleted';
